1bd74e24b73aea77a3d6cd0fce8fa2ea9f95f6ee
KatlynnGaster <kittygaster@gmail.com>
2024-09-25 10:57:32 -0600

    Added methods to classes

--- src/Character.java
+++ src/Character.java
@@ -1,2 +1,19 @@
+import javax.print.DocFlavor;
+
 public abstract class Character {
+
+    Room room;
+
+    /*
+    * Move characters around a room on a specific floor
+    */
+    public void move(Floor floor) {
+
+    }
+
+    public String toString(){
+        return "";
+    }
+
+
 }
